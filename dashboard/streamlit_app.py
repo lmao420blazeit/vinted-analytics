6dc74764783fc0f9e14bd2b354e662487bcfab51
lmao420blazeit <miguel98oliveira@gmail.com>
2024-02-03 16:08:31 +0000

    organized streamlit layout

--- dashboard/streamlit_app.py
+++ dashboard/streamlit_app.py
@@ -4,17 +4,19 @@ from sqlalchemy import create_engine
 import plotly.express as px
 import seaborn as sns
 import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+
 
 # Load a sample dataset
 def load_data():
     engine = create_engine('postgresql://user:4202@localhost:5432/vinted-ai')
-    sql_query = "SELECT * FROM public.products_catalog"
+    sql_query = "SELECT * FROM public.products_catalog LIMIT 5000"
     df = pd.read_sql(sql_query, engine)
     return (df)
 
 # Main function
 def main():
-    st.title('Interactive Streamlit Dashboard')
+    st.set_page_config(layout="wide")
 
     # Load data
     global products_catalog
@@ -25,54 +27,90 @@ def main():
         q_low = st.session_state.products_catalog["price"].quantile(0.05)
         st.session_state.products_catalog = st.session_state.products_catalog[(st.session_state.products_catalog["price"] < q_high) & 
                                                                               (st.session_state.products_catalog["price"] > q_low)]
+        
+    st.title("Brands")
 
-    fig = px.box(st.session_state.products_catalog, 
-                       x="price", 
-                       color="catalog_id")
-    st.plotly_chart(fig)    
+    price = st.session_state.products_catalog.groupby(["brand_title"])["price"].sum()
+    price = price/price.sum()
 
-    fig = px.pie(st.session_state.products_catalog, 
-                 values="price", 
-                 names="catalog_id",
-                 title= "Sum (prices) per catalog_id")
-    st.plotly_chart(fig) 
+    products = st.session_state.products_catalog.groupby(["brand_title"])["product_id"].count()
+    products = products/products.sum()
+    fig = go.Figure(data=[
+        go.Bar(name='Price', 
+               x=price.sort_values(ascending = False).head(20).reset_index()["brand_title"], 
+               y=price.sort_values(ascending = False).head(20).reset_index()["price"]
+               ),
+        go.Bar(name='Count', 
+               x=products.sort_values(ascending = False).head(20).reset_index()["brand_title"], 
+               y=products.sort_values(ascending = False).head(20).reset_index()["product_id"])
+    ])
+    fig.update_layout(title = "Normalized price and count")
+    st.plotly_chart(fig, use_container_width=True) 
 
-    fig = px.pie(st.session_state.products_catalog.groupby(["catalog_id"])["product_id"].count().reset_index(), #.rename_axis("limbs", axis="columns"), 
-                 values="product_id", 
-                 names="catalog_id",
-                 title= "Count (products) per catalog_id")
-    st.plotly_chart(fig) 
+    col1, col2 = st.columns([0.7, 0.3])
 
-    fig = px.pie(st.session_state.products_catalog.groupby(["size_title"])["price"].sum().sort_values(ascending = False).head(10).reset_index(), 
-                 values="price", 
-                 names="size_title",
-                 title= "Sum (price) per size_title")
-    st.plotly_chart(fig) 
+    # do median with boxplot
+    # do stacked bar with percentage of total count and price
 
-    fig = px.pie(st.session_state.products_catalog.groupby(["size_title"])["product_id"].count().sort_values(ascending = False).head(10).reset_index(),
-                 values="product_id", 
-                 names="size_title",
-                 title= "Count (products) per size_title")
-    st.plotly_chart(fig) 
+    brands = st.session_state.products_catalog.groupby(["brand_title"])["product_id"].count().sort_values(ascending = False).head(20).reset_index()["brand_title"]
 
-    st.table(st.session_state.products_catalog.groupby(["size_title"])["price"].mean().sort_values(ascending = False).head(10).reset_index())
-
-    fig = px.pie(st.session_state.products_catalog.groupby(["brand_title"])["price"].sum().sort_values(ascending = False).head(10).reset_index(), 
-                 values="price", 
-                 names="brand_title",
-                 title= "Sum (price) per brand_title")
-    st.plotly_chart(fig) 
-
-    fig = px.pie(st.session_state.products_catalog.groupby(["brand_title"])["product_id"].count().sort_values(ascending = False).head(10).reset_index(),
-                 values="product_id", 
-                 names="brand_title",
-                 title= "Count (products) per brand_title")
-    st.plotly_chart(fig) 
-
-    st.table(st.session_state.products_catalog.groupby(["brand_title"])["price"].mean().sort_values(ascending = False).head(10).reset_index())
-
+    fig = px.box(st.session_state.products_catalog[st.session_state.products_catalog["brand_title"].isin(brands)], 
+                       y="price", 
+                       color="brand_title",
+                       orientation="v",
+                       title= "Brand boxplot")
     
+    with col1:
+        st.plotly_chart(fig, use_container_width=True) 
 
+    fig = px.pie(price, 
+                 values=price.sort_values(ascending = False).head(20).reset_index()["price"], 
+                 names=price.sort_values(ascending = False).head(20).reset_index()["brand_title"], 
+                 title='Market share')
+    with col2:
+        st.plotly_chart(fig, use_container_width=True) 
+
+    st.title("Status")
+
+    col1, col2 = st.columns([0.7, 0.3])
+
+    price = st.session_state.products_catalog.groupby(["status"])["price"].sum()
+    price = price/price.sum()
+
+    products = st.session_state.products_catalog.groupby(["status"])["product_id"].count()
+    products = products/products.sum()
+    fig = go.Figure(data=[
+        go.Bar(name='Price', 
+               x=price.sort_values(ascending = False).head(20).reset_index()["status"], 
+               y=price.sort_values(ascending = False).head(20).reset_index()["price"]
+               ),
+        go.Bar(name='Count', 
+               x=products.sort_values(ascending = False).head(20).reset_index()["status"], 
+               y=products.sort_values(ascending = False).head(20).reset_index()["product_id"])
+    ])
+    fig.update_layout(title = "Normalized price and count")
+    with col1:
+        st.plotly_chart(fig, use_container_width=True) 
+
+    fig = px.pie(price, 
+                 values=price.sort_values(ascending = False).head(20).reset_index()["price"], 
+                 names=price.sort_values(ascending = False).head(20).reset_index()["status"], 
+                 title='Market share')
+    with col2:
+        st.plotly_chart(fig, use_container_width=True) 
+
+    # do median with boxplot
+    # do stacked bar with percentage of total count and price
+
+    brands = st.session_state.products_catalog.groupby(["status"])["product_id"].count().sort_values(ascending = False).head(20).reset_index()["status"]
+    print(brands)
+
+    fig = px.box(st.session_state.products_catalog[st.session_state.products_catalog["status"].isin(brands)], 
+                       y="price", 
+                       color="status",
+                       orientation="v",
+                       title= "Status-price boxplot")
+    st.plotly_chart(fig, use_container_width=True)   
 
 
 if __name__ == "__main__":
